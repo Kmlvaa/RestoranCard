a0c4529eecfbe487df2c9c1ac9719cda0129352f
Kmlvaa <samiraak@code.edu.az>
2025-05-20 16:25:48 +0400

    alt

--- src/_components/RestoranCard.jsx
+++ src/_components/RestoranCard.jsx
@@ -8,7 +8,7 @@ export default function RestoranCard({ name, imageUrl, cuisine, reytinq }) {
             <div className='CardHeader'>
                 <div className='ImageCover'>
                     <div className='ImageBorder'>
-                        <img src={imageUrl} className='CardImage' />
+                        <img src={imageUrl} alt='image' className='CardImage' />
                     </div>
                 </div>
             </div>
