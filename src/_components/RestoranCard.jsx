d6a3ca9e467d0142b865394c92360ffca5e3bfbc
Kmlvaa <samiraak@code.edu.az>
2025-05-20 17:51:58 +0400

    responsive design

--- src/_components/RestoranCard.jsx
+++ src/_components/RestoranCard.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RestoranCard.css'
 import { BiRestaurant } from "react-icons/bi";
+import data from '../_mockData/Data'
 
 export default function RestoranCard({ name, imageUrl, cuisine, reytinq }) {
+
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        const restoran = data.find(x => x.name === name);
+        setMenu(restoran?.menu || []);
+    }, [name]);
+
     return (
         <div className='Card'>
             <div className='CardHeader'>
@@ -18,6 +27,21 @@ export default function RestoranCard({ name, imageUrl, cuisine, reytinq }) {
                 <p>{cuisine}</p>
                 <p>{reytinq} / 5</p>
             </div>
+            <section className='Menu'>
+                <div className='MenuContent'>
+                    <h1>National Cuisine</h1>
+                    <ul>
+                        {menu?.map((m) => {
+                            return (
+                                <li id={m.id}>
+                                    <p>{m.name}</p>
+                                    <p>{m.price} $</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </section>
         </div>
     )
 }
--- src/_mockData/Data.js
+++ src/_mockData/Data.js
@@ -4,21 +4,102 @@ const restoranlar = [
         name: 'Borani',
         reytinq: 4,
         imageUrl: '/assets/restoranImage1.jpeg',
-        cuisine: 'Azerbaijan cuisine'
+        cuisine: 'Azerbaijan cuisine',
+        menu: [
+            {
+                id: 1,
+                name: 'Sac toyuq',
+                price: 18
+            },
+            {
+                id: 2,
+                name: 'Sac quzu',
+                price: 26
+            },
+            {
+                id: 3,
+                name: 'Sac can əti',
+                price: 28
+            },
+            {
+                id: 4,
+                name: 'Yarpaq dolması',
+                price: 8
+            },
+            {
+                id: 5,
+                name: 'Quzu qovurması',
+                price: 12
+            },
+        ]
     },
     {
         id: 2,
         name: 'Aiensk',
         reytinq: 5,
         imageUrl: '/assets/restoranImage2.jpeg',
-        cuisine: 'German cuisine'
+        cuisine: 'German cuisine',
+        menu: [
+            {
+                id: 1,
+                name: 'Sac toyuq',
+                price: 18
+            },
+            {
+                id: 2,
+                name: 'Sac quzu',
+                price: 26
+            },
+            {
+                id: 3,
+                name: 'Sac can əti',
+                price: 28
+            },
+            {
+                id: 4,
+                name: 'Yarpaq dolması',
+                price: 8
+            },
+            {
+                id: 5,
+                name: 'Quzu qovurması',
+                price: 12
+            },
+        ]
     },
     {
         id: 3,
         name: 'Mokichan',
         reytinq: 3,
         imageUrl: '/assets/restoranImage3.jpeg',
-        cuisine: 'Chinese cuisine'
+        cuisine: 'Chinese cuisine',
+        menu: [
+            {
+                id: 1,
+                name: 'Sac toyuq',
+                price: 18
+            },
+            {
+                id: 2,
+                name: 'Sac quzu',
+                price: 26
+            },
+            {
+                id: 3,
+                name: 'Sac can əti',
+                price: 28
+            },
+            {
+                id: 4,
+                name: 'Yarpaq dolması',
+                price: 8
+            },
+            {
+                id: 5,
+                name: 'Quzu qovurması',
+                price: 12
+            },
+        ]
     },
 ]
 export default restoranlar;
